9d1bf5c34a3cd3f1582d3db0fb7c8a671f606d0f
clonewert <clonewert@gmail.com>
2022-06-30 13:04:02 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+  let A = String(n).split('');
+  let B = [];
+  let res;
+  if (A[0] === '-') {
+    A.shift();
+  }
+  for (let i = A.length - 1; i >= 0; i--) {
+    B.push(A[i])
+  }
+
+  if (B[0] === '0') {
+    B.shift()
+  }
+   res = B.join('');
+   return res;   
 }
